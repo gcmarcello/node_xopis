030b054636f06a539abc603282b7f4c43398902e
Marcello Coelho <marcellogcoelho@outlook.com>
2025-03-11 12:26:36 -0300

    feat: add dotenv

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastify from 'fastify';
 import userRoutes from './routes/users';
 import productRoutes from './routes/products';
 import orderRoutes from './routes/orders';
+import 'dotenv/config';
 
 const shouldLog = process.env.NODE_ENV !== 'test';
 const server = fastify({ logger: shouldLog });
