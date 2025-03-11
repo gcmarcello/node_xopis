3f6e6a5783512252416dac2e18258fcd27d2020d
Marcello Coelho <marcellogcoelho@outlook.com>
2025-03-11 17:18:21 -0300

    test: add testing and error handling

--- src/actions/orders/create.ts
+++ src/actions/orders/create.ts
@@ -1,6 +1,8 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import { Order, OrderStatus } from "../../models/Order";
 import { Product } from "../../models/Product";
+import { InvalidAmountError } from "src/errors/invalidAmount";
+import { NotFoundError } from "objection";
 
 type Request = FastifyRequest<{
     Body: {
@@ -17,40 +19,72 @@ export default async (
     { body: { customer_id, items } }: Request,
     reply: FastifyReply
 ) => {
-    const products = await Product.query().findByIds(items.map((item) => item.product_id));
+    try {
+        const orderItems = items ?? []
+        const products = orderItems ? await Product.query().findByIds(orderItems.map(item => item.product_id)) : [];
 
-    const total_paid = items.reduce((acc, item) => {
-        const product = products.find((product) => product.id === item.product_id);
-        return acc + (product?.price || 0) * item.quantity;
-    }, 0);
+        if (products.length !== orderItems.length) {
+            throw new NotFoundError({message: 'Product not found'});
+        }
 
-    const total_discount = items.reduce((acc, item) => {
-        return acc + (item.discount || 0);
-    }, 0);
+        const productPriceMap = new Map(products.map(product => [product.id, product.price]));
 
-    // in this example, we are not calculating shipping and tax
-    const total_shipping = 0;
-    const total_tax = 0;
+        let total_price = 0;
+        let total_discount = 0;
+        let total_shipping = 0;
+        let total_tax = 0;
 
-    return Order.query().insertGraph({
-        customer_id,
-        items: items.map(item => ({
-            product_id: item.product_id,
-            quantity: item.quantity,
-            discount: item.discount || 0,
-            paid: 0,
-            tax: 0,
-            shipping: 0,
-        })),
-        total_discount,
-        total_paid,
-        total_shipping,
-        total_tax,
-        status: OrderStatus.PaymentPending
-    })
-        .then((order) => reply.code(201).send(order))
-        .catch((error) => {
-            return reply.send(error);
+
+        const orderItemsData = orderItems.map(item => {
+            if (item.discount && item.discount < 0) throw new InvalidAmountError(`Invalid amount for discount on product ${item.product_id}`)
+            if (item.quantity < 0) throw new InvalidAmountError(`Invalid amount for quantity on product ${item.product_id}`)
+            const price = productPriceMap.get(item.product_id) || 0;
+            const itemDiscount = item.discount || 0;
+            const itemTax = 0;
+            const itemShipping = 0;
+            const itemTotal = (price * item.quantity)
+            const itemPaid = Math.max(itemTotal - itemDiscount, 0)
+
+
+            total_price += Math.max((itemTotal + itemTax + itemShipping) - itemDiscount, 0);
+            total_discount += itemDiscount;
+            total_shipping += itemShipping;
+            total_tax += itemTax;
+
+            return {
+                product_id: item.product_id,
+                quantity: item.quantity,
+                discount: itemDiscount,
+                paid: itemPaid,
+                tax: itemTax,
+                shipping: itemShipping,
+            };
         });
+
+
+        const order = await Order.query().insertGraph({
+            customer_id,
+            total_paid: total_price,
+            total_discount,
+            total_shipping,
+            total_tax,
+            items: orderItemsData,
+            status: OrderStatus.PaymentPending
+        })
+
+        return reply.code(201).send(order);
+    } catch (error) {
+        
+        if (error instanceof InvalidAmountError) {
+            return reply.code(400).send(error);
+        }
+        if (error instanceof NotFoundError) {
+            return reply.code(400).send({message: error.message});
+        }
+        return reply.send(error);
+    };
 }
+
+
+
 
---
+++ tests/requests/orders/create.test.ts
@@ -0,0 +1,304 @@
+import 'tests/setup';
+import server from 'src/server';
+import Product from 'src/models/Product';
+import { LightMyRequestResponse } from 'fastify';
+import { Order, OrderItem, OrderStatus } from 'src/models';
+
+describe('CREATE action', () => {
+    const item1 = new OrderItem();
+    item1.product_id = 1;
+    item1.quantity = 2;
+    item1.discount = 0;
+
+    const item2 = new OrderItem();
+    item2.product_id = 2;
+    item2.quantity = 1;
+    item2.discount = 5;
+
+    const validInput: Partial<Order> = {
+        "customer_id": 1,
+        "items": [
+            item1,
+            item2
+        ]
+    }
+
+    beforeEach(async () => {
+        await createBeachBallProduct();
+        await createFishingRodProduct();
+    })
+
+
+
+    describe('when the input is valid', () => {
+        const input = validInput;
+
+        it('creates a new record', async () => {
+            await assertCount(input, { changedBy: 1 });
+        });
+
+        it('is successful', async () => {
+            const response = await makeRequest(input);
+            expect(response.statusCode).toBe(201);
+        });
+
+        it('returns the created order', async () => {
+            const response = await makeRequest(input);
+            const jsonResponse = response.json<Order>();
+            expect(jsonResponse).toEqual(
+                expect.objectContaining({
+                    "customer_id": 1,
+                    "id": 1,
+                    "total_paid": ((2.99 * 2) + 50) - 5,
+                    "total_tax": 0,
+                    "total_shipping": 0,
+                    "total_discount": 5,
+                    "status": "payment_pending",
+                    "items": [
+                        {
+                            "id": 1,
+                            "order_id": 1,
+                            "product_id": 1,
+                            "quantity": 2,
+                            "tax": 0,
+                            "shipping": 0,
+                            "discount": 0,
+                            "paid": 2.99 * 2 - 0
+                        },
+                        {
+                            "id": 2,
+                            "order_id": 1,
+                            "product_id": 2,
+                            "quantity": 1,
+                            "tax": 0,
+                            "shipping": 0,
+                            "discount": 5,
+                            "paid": 50 * 1 - 5
+                        }
+                    ]
+                })
+            );
+        });
+    });
+
+    describe('when the customer_id is missing', () => {
+        const { customer_id, ...input } = validInput;
+
+        it('does not create a new record', async () => {
+            await assertCount(input, { changedBy: 0 });
+        });
+
+        it('returns a bad request response', async () => {
+            const response = await makeRequest(input);
+
+            await assertBadRequest(response, /must have required property 'customer_id'/);
+        });
+    })
+
+    describe('when items are missing', () => {
+        const { items, ...input } = validInput;
+
+        it('creates a new record', async () => {
+            await assertCount(input, { changedBy: 1 });
+        });
+
+        it('is successful', async () => {
+            const response = await makeRequest(input);
+            expect(response.statusCode).toBe(201);
+        });
+
+        it('returns the created order', async () => {
+            const response = await makeRequest(input);
+            const jsonResponse = response.json<Order>();
+            expect(jsonResponse).toEqual(
+                expect.objectContaining({
+                    "customer_id": 1,
+                    "id": 1,
+                    "total_paid": 0,
+                    "total_tax": 0,
+                    "total_shipping": 0,
+                    "total_discount": 0,
+                    "status": "payment_pending",
+                    "items": []
+                })
+            );
+        });
+    })
+
+    describe('when item has a larger discount than its product price', () => {
+        const itemWithDiscountLargerThanValue = new OrderItem()
+        itemWithDiscountLargerThanValue.product_id = 2
+        itemWithDiscountLargerThanValue.quantity = 1
+        itemWithDiscountLargerThanValue.discount = 200;
+
+        const validInput: Partial<Order> = {
+            "customer_id": 1,
+            "items": [
+                itemWithDiscountLargerThanValue
+            ]
+        }
+
+        const input = validInput
+
+
+
+        it('creates a new record', async () => {
+            await assertCount(input, { changedBy: 1 });
+        });
+
+        it('is successful', async () => {
+            const response = await makeRequest(input);
+            expect(response.statusCode).toBe(201);
+        });
+
+        it('returns the created order with 0 paid value', async () => {
+            const response = await makeRequest(input);
+            const jsonResponse = response.json<Order>();
+            expect(jsonResponse).toEqual(
+                expect.objectContaining({
+                    "customer_id": 1,
+                    "id": 1,
+                    "total_paid": 0,
+                    "total_tax": 0,
+                    "total_shipping": 0,
+                    "total_discount": 200,
+                    "status": "payment_pending",
+                    "items": [
+                        {
+                            "id": 1,
+                            "order_id": 1,
+                            "product_id": 2,
+                            "quantity": 1,
+                            "tax": 0,
+                            "shipping": 0,
+                            "discount": 200,
+                            "paid": 0
+                        }
+                    ]
+                })
+            );
+        });
+    })
+
+    describe('when item has a negative quantity', () => {
+        const itemWithNegativeDiscount = new OrderItem()
+        itemWithNegativeDiscount.product_id = 1
+        itemWithNegativeDiscount.quantity = -1
+        itemWithNegativeDiscount.discount = 0;
+
+        const validInput: Partial<Order> = {
+            "customer_id": 1,
+            "items": [
+                itemWithNegativeDiscount
+            ]
+        }
+
+        const input = validInput
+
+        it('does not create a new record', async () => {
+            await assertCount(input, { changedBy: 0 });
+        });
+
+        it('returns a bad request response', async () => {
+            const response = await makeRequest(input);
+
+            await assertBadRequest(response, /Invalid amount for quantity on product 1/);
+        });
+    })
+
+    describe('when item has a negative discount', () => {
+        const itemWithNegativeDiscount = new OrderItem()
+        itemWithNegativeDiscount.product_id = 1
+        itemWithNegativeDiscount.quantity = 1
+        itemWithNegativeDiscount.discount = -1;
+
+        const validInput: Partial<Order> = {
+            "customer_id": 1,
+            "items": [
+                itemWithNegativeDiscount
+            ]
+        }
+
+        const input = validInput
+
+        it('does not create a new record', async () => {
+            await assertCount(input, { changedBy: 0 });
+        });
+
+        it('returns a bad request response', async () => {
+            const response = await makeRequest(input);
+            await assertBadRequest(response, /Invalid amount for discount on product 1/);
+        });
+    })
+
+    describe('when product does not exist', () => {
+        const itemWithNegativeDiscount = new OrderItem()
+        itemWithNegativeDiscount.product_id = 100
+        itemWithNegativeDiscount.quantity = 1
+        itemWithNegativeDiscount.discount = 0;
+
+        const validInput: Partial<Order> = {
+            "customer_id": 1,
+            "items": [
+                itemWithNegativeDiscount
+            ]
+        }
+
+        const input = validInput
+
+        it('does not create a new record', async () => {
+            await assertCount(input, { changedBy: 0 });
+        });
+
+        it('returns a bad request response', async () => {
+            const response = await makeRequest(input);
+            await assertBadRequest(response, /Product not found/);
+        });
+    })
+
+    const makeRequest = async (input: Partial<Order>) =>
+        server.inject({
+            method: 'POST',
+            url: '/orders',
+            body: input,
+        });
+
+    const countRecords = async () =>
+        Order.query().resultSize();
+
+    const assertCount = async (input: Partial<Order>, { changedBy }: { changedBy: number }) => {
+        const initialCount = await countRecords();
+
+        await makeRequest(input);
+
+        const finalCount = await countRecords();
+
+        expect(finalCount).toBe(initialCount + changedBy);
+    };
+
+    const assertBadRequest = async (response: LightMyRequestResponse, message: RegExp | string) => {
+        const json_response = response.json<{ message: string }>();
+        expect(response.statusCode).toBe(400);
+        expect(json_response.message).toMatch(message);
+    };
+
+    const createBeachBallProduct = async () =>
+        await Product.query()
+            .insert({
+                name: 'Beach Ball',
+                sku: 'BCHBLL',
+                description: 'Fun for the whole family',
+                price: 2.99,
+                stock: 100,
+            });
+
+    const createFishingRodProduct = async () =>
+        await Product.query()
+            .insert({
+                name: 'Fish Rod',
+                sku: 'FSHRD',
+                description: 'Fun for the whole family',
+                price: 50,
+                stock: 100,
+            });
+});
