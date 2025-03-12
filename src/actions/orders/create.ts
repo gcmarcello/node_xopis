77c34d4c0c18efab94570b63d2f46b6bf2892704
Marcello Coelho <marcellogcoelho@outlook.com>
2025-03-12 12:04:39 -0300

    refactor: use knex querybuilder to perform 2 queries only

--- src/actions/orders/create.ts
+++ src/actions/orders/create.ts
@@ -1,8 +1,9 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import { Order, OrderStatus } from "../../models/Order";
 import { Product } from "../../models/Product";
-import { InvalidAmountError } from "src/errors/invalidAmount";
+import { InvalidAmountError } from "../../errors/invalidAmount";
 import { NotFoundError } from "objection";
+import { OrderItem } from "../../models";
 
 type Request = FastifyRequest<{
     Body: {
@@ -30,58 +31,77 @@ export default async (
 
         const productPriceMap = new Map(products.map(product => [product.id, product.price]));
 
-        let total_price = 0;
+        let total_paid = 0;
         let total_discount = 0;
         let total_shipping = 0;
         let total_tax = 0;
 
-
-        const orderItemsData = orderItems.map(item => {
-            if (item.discount && item.discount < 0) throw new InvalidAmountError(`Invalid amount for discount on product ${item.product_id}`)
-            if (item.quantity < 0) throw new InvalidAmountError(`Invalid amount for quantity on product ${item.product_id}`)
+        for (const item of orderItems) {
             const price = productPriceMap.get(item.product_id) || 0;
             const itemDiscount = item.discount || 0;
             const itemTax = 0;
             const itemShipping = 0;
             const itemTotal = (price * item.quantity)
-            const itemPaid = Math.max(itemTotal - itemDiscount, 0)
 
+            if (item.discount && item.discount < 0) throw new InvalidAmountError(`Invalid amount for discount on product ${item.product_id}`)
+            if (item.quantity < 0) throw new InvalidAmountError(`Invalid amount for quantity on product ${item.product_id}`)
 
-            total_price += Math.max((itemTotal + itemTax + itemShipping) - itemDiscount, 0);
+            total_paid += Math.max((itemTotal + itemTax + itemShipping) - itemDiscount, 0);
             total_discount += itemDiscount;
             total_shipping += itemShipping;
             total_tax += itemTax;
-
-            return {
-                product_id: item.product_id,
-                quantity: item.quantity,
-                discount: itemDiscount,
-                paid: itemPaid,
-                tax: itemTax,
-                shipping: itemShipping,
-            };
-        });
+        }
 
 
-        const order = await Order.query().insertGraph({
-            customer_id,
-            total_paid: total_price,
-            total_discount,
-            total_shipping,
-            total_tax,
-            items: orderItemsData,
-            status: OrderStatus.PaymentPending
+        const order = await Order.transaction(async (trx) => {
+            const order = await Order.query(trx).insertGraph({
+                customer_id,
+                total_paid,
+                total_discount,
+                total_shipping,
+                total_tax,
+                status: OrderStatus.PaymentPending
+            });
+
+            const orderItemsData = orderItems.map(item => {
+                const price = productPriceMap.get(item.product_id) || 0;
+                const itemDiscount = item.discount || 0;
+                const itemTax = 0;
+                const itemShipping = 0;
+                const itemTotal = (price * item.quantity)
+                const itemPaid = Math.max(itemTotal - itemDiscount, 0)
+
+
+
+                return {
+                    order_id: order.id,
+                    product_id: item.product_id,
+                    quantity: item.quantity,
+                    discount: itemDiscount,
+                    paid: itemPaid,
+                    tax: itemTax,
+                    shipping: itemShipping,
+                };
+            });
+
+            const items = orderItemsData.length ? await OrderItem.knex()
+                .batchInsert("orders_items", orderItemsData, 100)
+                .transacting(trx)
+                // as any needed due to a bug in objection types
+                .returning(['discount', 'id', 'order_id', 'paid', 'product_id', 'quantity', 'shipping', 'tax'] as any[]) : [];
+
+            return { ...order, items }
         })
 
         return reply.code(201).send(order);
     } catch (error) {
-        
         if (error instanceof InvalidAmountError) {
             return reply.code(400).send(error);
         }
         if (error instanceof NotFoundError) {
-            return reply.code(400).send({message: error.message});
+            return reply.code(400).send({ message: error.message });
         }
+        console.log(error)
         return reply.send(error);
     };
 }
