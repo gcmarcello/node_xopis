6624cf5cfaa9895e62d5ab07f0d3d55b8c14144b
Marcello Coelho <marcellogcoelho@outlook.com>
2025-03-13 12:09:39 -0300

    feat: add verifyIfItemsAreEqual function to compare order items with existing records

--- src/services/orderItems.service.ts
+++ src/services/orderItems.service.ts
@@ -1,5 +1,7 @@
 import { Order } from "src/models";
 import OrderItem, { OrderItemAttributes } from "../models/OrderItem";
+import { isDeepStrictEqual } from "node:util";
+import Objection from "objection";
 
 export function generateOrderItems(items: OrderItemAttributes[], order: Order, productPriceMap: Map<number, number>) {
     return items.map(item => {
@@ -20,4 +22,30 @@ export function generateOrderItems(items: OrderItemAttributes[], order: Order, p
             shipping: itemShipping,
         })
     });
+}
+
+export async function verifyIfItemsAreEqual(orderItems: OrderItemAttributes[], order_id: number, trx?: Objection.Transaction) {
+    const existingItems = await OrderItem.query(trx).where('order_id', order_id).select(['product_id', 'quantity', 'discount', 'tax', 'shipping']);
+
+    for (const item of orderItems) {
+        const existingItem = existingItems.find(i => i.product_id === item.product_id);
+        if (!existingItem) return false;
+        const existingItemDetails = {
+            quantity: existingItem.quantity,
+            product_id: existingItem.product_id,
+            discount: existingItem.discount,
+            tax: existingItem.tax,
+            shipping: existingItem.shipping
+        }
+        const newItem = {
+            quantity: item.quantity,
+            product_id: item.product_id,
+            discount: item.discount ?? 0,
+            tax: item.tax ?? 0,
+            shipping: item.shipping ?? 0
+        }
+        if(!isDeepStrictEqual(existingItemDetails, newItem)) return false;
+    }
+
+    return true;
 }
