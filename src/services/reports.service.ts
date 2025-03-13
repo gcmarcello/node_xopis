90da0e4b22ee8ce6236f1bfbbe3d34b3f8510b1d
Marcello Coelho <marcellogcoelho@outlook.com>
2025-03-13 13:35:25 -0300

    feat: enhance sales report functionality to include product details and handle missing products

--- src/services/reports.service.ts
+++ src/services/reports.service.ts
@@ -1,7 +1,24 @@
-import { OrderItem } from "src/models";
+import { OrderItem, Product } from "../models";
 
-export async function listSalesByDayAndProduct({start_date, end_date, product_id}:{start_date: string, end_date: string, product_id?: string}) {
-    const itemsSold = await OrderItem.query().whereBetween('created_at', [start_date, end_date])
+export async function listSalesByDayAndProduct({ start_date, end_date, product_id }: { start_date: string, end_date: string, product_id?: number }) {
+    const itemsSold = product_id ?
+        await OrderItem.query().where('product_id', product_id).whereBetween('created_at', [start_date, end_date]) :
+        await OrderItem.query().whereBetween('created_at', [start_date, end_date])
+        
+    const uniqueProducts = new Set(itemsSold.map(item => item.product_id))
 
-    return itemsSold
+    const products = await Product.query().findByIds([...uniqueProducts])
+    const productMap = new Map(products.map(product => [product.id, product]))
+
+    const parsedItems = itemsSold.map(item => {
+        const product = productMap.get(item.product_id)
+        if (!product) return null
+        return {
+            product_id: item.product_id,
+            quantity: item.quantity,
+            total_sold: (item.quantity * product.price) + item.tax + item.shipping - item.discount,
+        }
+    })
+
+    return parsedItems
 }
