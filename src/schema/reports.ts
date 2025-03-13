8a795c0bc79932ecc0baec1a2207ae4c9d2a7a61
Marcello Coelho <marcellogcoelho@outlook.com>
2025-03-13 18:45:21 -0300

    feat: add topsales schema

--- src/schema/reports.ts
+++ src/schema/reports.ts
@@ -1,9 +1,21 @@
 import z from "zod";
 
-export const salesReportSchema = z.object({
+export const datesSchema = z.object({
     start_date: z.coerce.date(),
     end_date: z.coerce.date(),
-    product_id: z.coerce.number().optional(),
 })
 
+export const salesReportSchema = z.object({
+    product_id: z.coerce.number().optional(),
+}).merge(datesSchema)
+
 export type SalesReportSchema = z.infer<typeof salesReportSchema>
+
+export const topProductsSchema = z.object({
+    breakdown: z.coerce
+        .string()
+        .transform(value => value.toLowerCase() === "true")
+        .pipe(z.boolean())
+})
+
+export type TopProductsSchema = z.infer<typeof topProductsSchema>
